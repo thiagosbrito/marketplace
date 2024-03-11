69e900c1ee652ef8e93e4b7ec2ee810f1ee0dd8d
Thiago Brito <thiago83brito@hotmail.com>
2024-03-11 16:46:47 +0100

    Trying to solve broken build

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -14,7 +14,9 @@ const authoptions = {
     credential: cert({
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
+                    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n")
+                    : undefined,
     })
  })
 };
