8942df289e1240cb0ab7530f1f848d96292b54cd
Thiago Brito <thiago83brito@hotmail.com>
2024-03-13 21:03:12 +0100

    Updating product page, featured products on home page

--- src/app/products/[category]/[subcategory]/[product]/page.tsx
+++ src/app/products/[category]/[subcategory]/[product]/page.tsx
@@ -13,7 +13,13 @@ import { useQuery } from "@tanstack/react-query";
 
 const ProductDetailsPage = () => {
     const productSlug = useParams().product as string;
-    const [productDetails, setProductDetails] = useState<any | null>(null);
+    const [productDetails, setProductDetails] = useState<{
+        name: string;
+        price: number;
+        description: string;
+        provider_name: string;
+        gallery: any;
+    } | null>(null);
     
     const { data, isLoading, isError, isSuccess } = useQuery({
         queryKey: ["product-details", productSlug],
@@ -39,6 +45,7 @@ const ProductDetailsPage = () => {
     //     }
     // }, [productSlug]);
     if (isLoading) return <p>Loading...</p>;
+    if (productDetails === null) return <p>Product not found</p>;
     return (
         <div className="py-6">
             <Breadcrumbs
@@ -52,23 +59,23 @@ const ProductDetailsPage = () => {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-6">
                 <div className="flex flex-col md:flex-row -mx-4">
                     <div className="md:flex-1 px-4">
-                        <ProductImageCarousel gallery={productDetails?.gallery} />
+                        <ProductImageCarousel gallery={productDetails.gallery} />
                     </div>
                     <div className="md:flex-1 px-4">
                         <h2 className="mb-2 leading-tight tracking-tight font-bold text-gray-800 text-2xl md:text-3xl">
-                            {productDetails?.name}
+                            {productDetails.name}
                         </h2>
                         <p className="text-gray-500 text-sm">
                             By{" "}
                             <a href="#" className="text-indigo-600 hover:underline">
-                                {productDetails?.provider_name}
+                                {productDetails.provider_name}
                             </a>
                         </p>
                         <div className="flex items-center space-x-4 my-4">
                             <div>
                                 <div className="rounded-lg bg-gray-100 flex py-2 px-3">
                                     <span className="text-indigo-400 mr-1 mt-1">R$</span>
-                                    <span className="font-bold text-indigo-600 text-3xl">{formatPrice(productDetails?.price).replace('R$', '')}</span>
+                                    <span className="font-bold text-indigo-600 text-3xl">{formatPrice(productDetails.price).replace('R$', '')}</span>
                                 </div>
                             </div>
                             <div className="flex-1">
@@ -77,7 +84,7 @@ const ProductDetailsPage = () => {
                             </div>
                         </div>
                         <div className="text-gray-500">
-                            <Markdown>{productDetails?.description}</Markdown>
+                            <Markdown>{productDetails.description}</Markdown>
                         </div>
                         <div className="flex py-4 space-x-4">
                             <div className="relative">
